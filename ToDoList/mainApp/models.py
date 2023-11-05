049756663ba96b21692a4cf1000296e28d976b42
Maiara Colombo <maiara.ccolombo@outlook.com>
2023-11-05 10:17:34 -0300

    ToDoList

---
+++ ToDoList/mainApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+# Create your models here.
+class Todo(models.Model):
+    Title = models.CharField(max_length=100, blank=False)
+    Completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.Title
